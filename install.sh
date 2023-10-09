fff91ec12541fecdec007149a061f7aaeba8f75f
Aaron Agarunov <agarunov.aaron@gmail.com>
2023-10-09 14:45:18 -0400

    Update files

--- install.sh
+++ install.sh
@@ -32,13 +32,17 @@ ln -sfn ~/.dotfiles/tmux/.tmux.conf ~
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # WSL2 / Linux
+  # VS Code
   rm -rf /mnt/c/Users/Aaron/AppData/Roaming/Code/User/settings.json
   cp ~/.dotfiles/vscode/settings.json /mnt/c/Users/Aaron/AppData/Roaming/Code/User/settings.json
+  # https://learn.microsoft.com/en-us/windows/wsl/tutorials/wsl-git
+  git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-wincred.exe"
 elif [ "$(uname)" == "Darwin" ]; then
   # MacOS
+  # VS Code
   rm -rf $HOME/Library/Application Support/Code/User/settings.json
   cp ~/.dotfiles/vscode/settings.json $HOME/Library/Application Support/Code/User/settings.json
-
+  # skhd
   ln -sfn ~/.dotfiles/.skhdrc ~
 fi
 
